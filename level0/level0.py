cbdf8dbe24289cca5a18d6fb987286a5aea1648e
yuokada <callistoiv@gmail.com>
2011-09-10 15:34:12 +0900

    move  directories.

---
+++ level0/level0.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+print "level 1's answer: ",
+print 2**38
--- level2/level2.py
+++ level2/level2.py
@@ -1,5 +1,21 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
+#!/usr/bin/python2.6
 
-print "level 1's answer: ",
-print 2**38
+import re
+
+def main():
+    """docstring for main"""
+    f   = open("ocr.html")
+    pat = re.compile("[a-z]")
+    answer_list = []
+    for line in f.readlines():
+        if pat.search(line) :
+            match =  pat.findall(line)
+            print match 
+            answer_list.extend(match)
+
+    return  "".join(answer_list)
+
+if __name__ == '__main__':
+    ans = main()
+    print "level 2's answer: ", 
+    print  ans

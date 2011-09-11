c5b878861cdac6c1a718161ca6b396fb5c011aff
yuokada <callistoiv@gmail.com>
2011-09-11 21:55:54 +0900

    complete  level5.

---
+++ level5/level5.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.6
+
+import pickle
+
+def main():
+    """docstring for main"""
+    pic = pickle.load(open("banner.p"))
+    for tup in pic:
+        print "".join([s[0]*s[1] for s in tup])
+
+if __name__ == '__main__':
+    ans = main()
+    print "level 5's answer: ", 
+    print  ans

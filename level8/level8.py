3394331ec506c88b12f3b3a1048a849d698d39a5
yuokada <callistoiv@gmail.com>
2011-09-12 01:46:03 +0900

    complete level8&9.

---
+++ level8/level8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.6
+
+import bz2
+
+def main():
+    un= 'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
+    pw= 'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
+    
+    print  "un:", bz2.decompress(un)
+    print  "pw:", bz2.decompress(pw)
+
+    return "print it." 
+
+if __name__ == '__main__':
+    ans = main()
+    print "level 6's answer: ", 
+    print  ans

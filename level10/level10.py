989466bf13019326b848d65a0b21c40bd0e3e2fb
yuokada <callistoiv@gmail.com>
2011-09-12 03:37:14 +0900

    complete  level10

--- level10/level10.py
+++ level10/level10.py
@@ -4,13 +4,15 @@
 import re
 
 def main():
-    """"""
-    a = [1,11,21,1211,111221,]
+    a = [1,]
     target = "1"
-    max_time =  31
+    max_time =  33
     for i in range(max_time):
         target =  count_converter(target)
-        print target
+        a.append(target)
+
+    for j in range(len(a)):
+        print j, len(str(a[j])), a[j]
 
 def count_converter(number):
     number = str(number)
@@ -18,9 +20,10 @@ def count_converter(number):
     pre_chr = number[0]
     counter = 0
     for i in range( len(number) ):
-        if i == len(number)-1: # slow
+        if i == (len(number)-1): # slow
             if pre_chr == number[i]:
-                result.append((pre_chr, counter))
+                counter += 1
+                result.append([pre_chr, counter])
             else:
                 result.append([pre_chr, counter])
                 result.append([number[i], 1])
@@ -28,15 +31,11 @@ def count_converter(number):
             if pre_chr == number[i]:
                 counter += 1
             else:
-                result.append((pre_chr, counter))
+                result.append([pre_chr, counter])
                 counter = 1
                 pre_chr = number[i]
     #print  result
     return  "".join([str(y)+str(x) for x,y in result])
 
-
 if __name__ == '__main__':
     main()      
-    count_converter(1)
-    ### get  tuple  ->  create string.
-    ### (n, len)    ->  "n"*len

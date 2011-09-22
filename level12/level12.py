59e13ada88ad43212cb60498fb5e020671e9c654
yuokada <callistoiv@gmail.com>
2011-09-23 06:29:33 +0900

    clear  lv12 & lv13.

---
+++ level12/level12.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python2.6
+
+f = open("./evil2.gfx").read()
+
+for i in range(5):
+    open("lv12_%d.jpg" % i ,"w").write(f[i::5])
+
---
+++ level13/level13.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2.6
+# -*- coding:utf-8 -*-
+
+import xmlrpclib
+
+url= "http://www.pythonchallenge.com/pc/phonebook.php"
+server= xmlrpclib.ServerProxy(url)
+
+#print server.system.listMethods()
+#print "phone method  is " , server.system.methodSignature("phone")
+
+print server.phone("Bert")

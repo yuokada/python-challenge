d6fdb6bfa25c394fc81fdb7cb37750b6a83bb3b4
yuokada <callistoiv+git@gmail.com>
2018-04-18 00:15:54 +0900

    Reformat

--- level07/level7.py
+++ level07/level7.py
@@ -3,23 +3,25 @@
 import re
 import PIL.Image
 
+
 def main():
     img = PIL.Image.open("oxygen.png")
-    
-    length_x,length_y = img.size      #(629,95)
-    #print img.format    # PNG
-    for x in range(0,length_x,7):
+
+    length_x, length_y = img.size  # (629,95)
+    # print img.format    # PNG
+    for x in range(0, length_x, 7):
         for y in range(length_y):
-            if y == length_y/2:
-                r,g,b,a = img.getpixel((x,y))
-                if  r == g == b :
-                    #print x,y ,img.getpixel((x,y))
+            if y == length_y / 2:
+                r, g, b, a = img.getpixel((x, y))
+                if r == g == b:
+                    # print x,y ,img.getpixel((x,y))
                     print chr(r),
-    print 
-    answers =[105,110,116,101,103,114,105,116,121]
-    return "".join([ chr(s) for  s in answers])
+    print
+    answers = [105, 110, 116, 101, 103, 114, 105, 116, 121]
+    return "".join([chr(s) for s in answers])
+
 
 if __name__ == '__main__':
     ans = main()
-    print "level 6's answer: ", 
-    print  ans
+    print "level 6's answer: ",
+    print ans
